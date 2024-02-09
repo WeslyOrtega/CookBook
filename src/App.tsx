2a7cf8aa5872e6a8630efc2fd0d2bdd652ca9d53
Wesly Ortega <weslyortega@gmail.com>
2024-02-09 00:12:27 -0800

    Designed basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
+import Home from "./pages/home/Home";
+
 function App() {
+  const NavLink = (props: { href: string; children: string }) => (
+    <li className="hover:bg-primary-dark transition-colors p-1">
+      <a href={props.href}>{props.children}</a>
+    </li>
+  );
+
   return (
-    <div>
-      <header>
-        <p className="text-2xl">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-row">
+      <nav className="flex flex-col items-center w-72 h-screen bg-primary p-7 gap-7">
+        <h1 className="text-2xl font-semibold">CookBook</h1>
+        <div className="w-full">
+          <input className="w-full" type="text" placeholder="Search" />
+        </div>
+        <ul className="w-full text-xl font-semibold flex flex-col gap-2">
+          <NavLink href=".">Add a recipe</NavLink>
+          <NavLink href=".">Home</NavLink>
+          <NavLink href=".">Recipes</NavLink>
+          <NavLink href=".">About</NavLink>
+        </ul>
+      </nav>
+      <article className="bg-primary-lighter w-full p-10">
+        <Home />
+      </article>
     </div>
   );
 }
