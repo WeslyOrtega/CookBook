5851ddbe957e1d702b92a67d61a66282eb9cb4fc
Wesly Ortega <weslyortega@gmail.com>
2024-03-01 00:38:56 -0800

    Add theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxHamburgerMenu, RxSun, RxMoon } from "react-icons/rx";
 
 import Home from "./pages/home/Home";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
 
 const SideDrawerLinks: Array<{ href: string; text: string }> = [
   {
@@ -21,44 +23,60 @@ const SideDrawerLinks: Array<{ href: string; text: string }> = [
   },
 ];
 
-const NavWrapper = (props: { children: React.JSX.Element }) => (
-  <div className="drawer">
-    <input id="side-drawer" className="drawer-toggle" type="checkbox" />
-    <div className="drawer-side z-10">
-      <label htmlFor="side-drawer" className="drawer-overlay"></label>
-      <ul className="menu p-4 w-80 min-h-full bg-base-100">
-        {SideDrawerLinks.map(({ href, text }, index) => (
-          <li key={index}>
-            <a href={href}>{text}</a>
-          </li>
-        ))}
-      </ul>
+const NavWrapper = (props: { children: React.JSX.Element }) => {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+
+  return (
+    <div className="drawer">
+      <input id="side-drawer" className="drawer-toggle" type="checkbox" />
+      <div className="drawer-side z-10">
+        <label htmlFor="side-drawer" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-100">
+          {SideDrawerLinks.map(({ href, text }, index) => (
+            <li key={index}>
+              <a href={href}>{text}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="drawer-content">
+        <nav className="navbar bg-base-200">
+          <div>
+            <label
+              className="btn btn-ghost btn-square drawer-button btn-md p-2"
+              onClick={() => document.getElementById("side-drawer")?.click()}
+            >
+              <RxHamburgerMenu className="w-full h-full" />
+            </label>
+          </div>
+          <div className="navbar-start"></div>
+          <div className="navbar-center">
+            <a href="." className="btn no-animation btn-ghost text-xl">
+              Your CookBook
+            </a>
+          </div>
+          {/* Empty end to actually center navbar-center */}
+          <div className="navbar-end">
+            <label className="btn btn-circle shadow-none swap">
+              <input
+                type="checkbox"
+                value="dark"
+                className="theme-controller"
+              />
+              <RxSun className="swap-off fill-current size-6" />
+              <RxMoon className="swap-on fill-current size-6" />
+            </label>
+          </div>
+        </nav>
+        <main className="p-5 flex flex-col items-center">
+          <div className="max-w-screen-xl w-full">{props.children}</div>
+        </main>
+      </div>
     </div>
-    <div className="drawer-content">
-      <nav className="navbar bg-base-200">
-        <div>
-          <label
-            className="btn btn-ghost btn-square drawer-button btn-md p-2"
-            onClick={() => document.getElementById("side-drawer")?.click()}
-          >
-            <RxHamburgerMenu className="w-full h-full" />
-          </label>
-        </div>
-        <div className="navbar-start"></div>
-        <div className="navbar-center">
-          <a href="." className="btn no-animation btn-ghost text-xl">
-            Your CookBook
-          </a>
-        </div>
-        {/* Empty end to actually center navbar-center */}
-        <div className="navbar-end" />
-      </nav>
-      <main className="p-5 flex flex-col items-center">
-        <div className="max-w-screen-xl w-full">{props.children}</div>
-      </main>
-    </div>
-  </div>
-);
+  );
+};
 
 function App() {
   return (
