74d798551a9bd2e2f59930e2981cef0b951503b2
Wesly Ortega <weslyortega@gmail.com>
2024-03-05 23:45:19 -0800

    Add recipe creation route

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import {
 import Home from "./pages/home/Home";
 import ContentWrapper from "./components/Layout/ContentWrapper";
 import RecipeInfo from "./pages/recipe/RecipeInfo";
+import RecipeEnter from "./pages/recipe/RecipeEnter";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<ContentWrapper />}>
       <Route index element={<Home />} />
       <Route path="recipe">
+        <Route path="new" element={<RecipeEnter />} />
         <Route path=":id" element={<RecipeInfo />} />
       </Route>
     </Route>
---
+++ src/pages/recipe/RecipeEnter.tsx
@@ -0,0 +1,5 @@
+const RecipeEnter = () => {
+  return <p>Enter recipe</p>;
+};
+
+export default RecipeEnter;
