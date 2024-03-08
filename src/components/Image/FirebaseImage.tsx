74ba28e91dbafdbee9dfd75753b579d8d376bb3d
Wesly Ortega <weslyortega@gmail.com>
2024-03-08 01:07:21 -0800

    Distinguish between fetching url and fully loading img

--- src/components/Image/FirebaseImage.tsx
+++ src/components/Image/FirebaseImage.tsx
@@ -3,33 +3,45 @@ import { getRecipeImage } from "@/src/data/firebase";
 import { useEffect, useState } from "react";
 import { RxLinkBreak1 } from "react-icons/rx";
 
+enum ImgState {
+  loading,
+  error,
+  loaded,
+}
+
 const FirebaseImg = (props: { img_url: string; className?: string }) => {
   const { img_url, className } = props;
-  const [img, setImg] = useState("");
-
-  const image_load_error = "<LOAD_ERROR>";
+  const [firebase_url, setImg] = useState("");
+  const [img_state, setImageState] = useState(ImgState.loading);
 
   useEffect(() => {
     getRecipeImage(img_url)
       .then((it) => {
         setImg(it);
-        console.log(img);
       })
-      .catch(() => setImg(image_load_error));
+      .catch(() => setImageState(ImgState.error));
   }, [img_url]);
 
   return (
     <>
-      {img == "" && <Skeleton className={className} />}
-      {img == image_load_error && (
+      {img_state === ImgState.loading && (
+        <Skeleton className={className}>
+          <RxLinkBreak1 className="w-full h-full p-20 opacity-0" />
+        </Skeleton>
+      )}
+      {img_state === ImgState.error && (
         <div className={className}>
           <RxLinkBreak1 className="w-full h-full p-20" />
         </div>
       )}
-      {img != "" && img != image_load_error && (
-        <figure className={className}>
-          <img src={img} alt="Picture of recipe" />
-        </figure>
+      {img_state !== ImgState.error && (
+        <img
+          style={img_state === ImgState.loaded ? {} : { display: "none" }}
+          className={className}
+          src={firebase_url}
+          alt="Picture of recipe"
+          onLoad={() => setImageState(ImgState.loaded)}
+        />
       )}
     </>
   );
