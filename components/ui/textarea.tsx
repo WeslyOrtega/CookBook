129b6548ebfe0cc8e4e3308357689367e6afc0cf
Wesly Ortega <weslyortega@gmail.com>
2024-03-08 21:32:31 -0800

    Update autoexpand logic

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -4,15 +4,14 @@ import { cn } from "@/lib/utils";
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  expand?: boolean;
+  autoExpand?: boolean;
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, onInput, expand, ...props }, ref) => {
+  ({ className, onInput, autoExpand, ...props }, ref) => {
     const handleInput = (e: React.FormEvent<HTMLTextAreaElement>) => {
-      if (expand) {
-        // +2 to account for padding
-        e.currentTarget.style.height = e.currentTarget.scrollHeight + 2 + "px";
+      if (autoExpand) {
+        e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
       }
       if (onInput) onInput(e);
     };
@@ -21,6 +20,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       <textarea
         className={cn(
           "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          autoExpand ? "overflow-y-hidden resize-none" : "",
           className
         )}
         ref={ref}
